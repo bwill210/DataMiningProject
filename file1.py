dc6eecbcaaac0d3f7e683bab9ed8c06a6cb765ed
Brock Williams <bwill210@fiu.edu>
2022-04-13 00:19:22 -0400

    data cleaning
    binarized attributes "capital_gain", "capital_loss" and "country"

--- file1.py
+++ file1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import Binarizer
 import matplotlib.pyplot as plt
 
 
@@ -37,8 +38,8 @@ print(f"Number of categorical columns: {len(cat_cols)}\n")
 #create dependent and independent variable vectors
 x = dataset.iloc[:,:-1].values   #independent: all rows and colums except last column
 y = dataset.iloc[:,14].values   #dependent: >50k or <50k
-print(x)
-print(y)
+#print(x)
+#print(y)
 
 #removing unnecessary attributes
 to_drop = ['fnlwgt', 'education', 'relationship']
@@ -46,7 +47,7 @@ dataset.drop(columns=to_drop, inplace=True)
 
 #dropping records with missing data
 new_data = dataset.replace('?', np.NaN)
-#new_data = new_data.dropna(axis = 0, how ='any')   # new dataset with missing values dropped
+new_data = new_data.dropna(axis = 0, how ='any')   # new dataset with missing values dropped
 
 print("Old data frame length:", len(dataset))
 print("New data frame length:", len(new_data))
@@ -65,3 +66,12 @@ print(object_cols)
 #Display only categorical feature records
 features = new_data[object_cols]
 print(features.head())
+
+#binarizing attributes "capital_gain" "capital_loss" "country"
+new_data.loc[new_data["capital_gain"] > 0, "capital_gain"] = 1
+new_data.loc[new_data["capital_loss"] > 0, "capital_loss"] = 1
+new_data["country"] = (new_data["country"] == "United-States").astype(int) # 1 if country == United_States, 0 otherwise
+x = new_data.iloc[:,:-1].values
+print(x)
+
+#discretization of continuous attributes "age" and "hr_per_week"
